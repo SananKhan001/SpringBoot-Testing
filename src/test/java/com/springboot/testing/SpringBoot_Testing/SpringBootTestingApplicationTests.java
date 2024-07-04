6c69e9230a27c60b1b53bc09d892df8348ddea8f
sanan <sanan3946@gmail.com>
2024-07-04 17:51:35 +0530

    Repo init

---
+++ src/test/java/com/springboot/testing/SpringBoot_Testing/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testing.SpringBoot_Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
